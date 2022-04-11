09d9b2a7d9dce60529f4e39137a8fb6a884246a8
Timo Purzner <timo.purzner@codecamp-n.com>
2022-04-11 11:55:48 +0200

    init

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :note2s
+  resources :notes
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
